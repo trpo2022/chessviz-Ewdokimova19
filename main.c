2c5929889d3c48e5d4ac010a74de6dfb83f0ffad
Ewdokimova21 <viol.ewdokimowa@yandex.ru>
2022-02-19 14:27:40 -0800

    add comentaries in main.c

--- main.c
+++ main.c
@@ -1,44 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Show(char c[8][8]){
-    printf("   ");
-    for(int i=0;i<8;i++){
-            printf(" %c",'a'+i);
-        }
-    printf(" \n");
-
-    printf("   -");
-    for(int i=0;i<8;i++){
-           printf("--");
-        }
-    printf("\n");
-
-    for(int i=7;i>=0;i--){
-        printf("%d |",i+1);
-        for(int j=0;j<8;j++){
-            printf(" %c",c[i][j]);
-        }
-        printf(" | %d \n",i+1);
-    }
-
-    printf("   -");
-    for(int i=0;i<8;i++){
-            printf("--");
-        }
-    printf("\n");
-
-    printf("   ");
-    for(int i=0;i<8;i++){
-            printf(" %c",'a'+i);
-        }
-    printf("\n");
-
+Show (char c[8][8]) {
+	//a b c d e f g h - top
+	printf("   ");
+   for(int i=0;i<8;i++){
+      printf(" %c",'a'+i);
+   }
+   printf(" \n");
+   
+   // _ _ _ _ _ _ _ _
+   printf("   -");
+   for(int i=0;i<8;i++){
+      printf("--");
+   }
+   printf("\n");
+   
+   //1) 8-1+| 2) figur
+   for(int i=7;i>=0;i--){
+      printf("%d |",i+1);
+      for(int j=0;j<8;j++){
+         printf(" %c",c[i][j]);
+      }
+      printf(" | %d \n",i+1);
+   }
+    
+   // _ _ _ _ _ _ _ _ 
+   printf("   -");
+   for(int i=0;i<8;i++){
+      printf("--");
+   }
+   printf("\n");
+   
+	//a b c d e f g h - buttom
+   printf("   ");
+   for(int i=0;i<8;i++){
+      printf(" %c",'a'+i);
+   }
+   printf(" \n");
 }
 
-int main()
-{
-    char board[8][8]={
+main () {
+	char board[8][8]={
     {'R','N','B','Q','K','B','N','R'},
     {'P','P','P','P','P','P','P','P'},
     {' ',' ',' ',' ',' ',' ',' ',' '},
