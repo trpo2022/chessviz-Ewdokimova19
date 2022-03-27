f037ae62a32e302c0512748867e2c3ff9382296c
Ewdokimova21 <viol.ewdokimowa@yandex.ru>
2022-03-27 22:45:29 +0700

    edit main.c

--- main.c
+++ main.c
@@ -1,56 +1,27 @@
+#include <libchessviz/chessviz.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-Show (char c[8][8]) {
-	//a b c d e f g h - top
-	printf("   ");
-   for(int i=0;i<8;i++){
-      printf(" %c",'a'+i);
-   }
-   printf(" \n");
-   
-   // _ _ _ _ _ _ _ _
-   printf("   -");
-   for(int i=0;i<8;i++){
-      printf("--");
-   }
-   printf("\n");
-   
-   //1) 8-1+| 2) figur
-   for(int i=7;i>=0;i--){
-      printf("%d |",i+1);
-      for(int j=0;j<8;j++){
-         printf(" %c",c[i][j]);
-      }
-      printf(" | %d \n",i+1);
-   }
-    
-   // _ _ _ _ _ _ _ _ 
-   printf("   -");
-   for(int i=0;i<8;i++){
-      printf("--");
-   }
-   printf("\n");
-   
-	//a b c d e f g h - buttom
-   printf("   ");
-   for(int i=0;i<8;i++){
-      printf(" %c",'a'+i);
-   }
-   printf(" \n");
-}
+int main()
+{
+    char board[9][9]
+            = {"8rnbqkbnr",
+               "7pppppppp",
+               "6........",
+               "5........",
+               "4........",
+               "3........",
+               "2PPPPPPPP",
+               "1RNBQKBNR",
+               " abcdefgh"};
+    char input[7];
 
-main () {
-	char board[8][8]={
-    {'R','N','B','Q','K','B','N','R'},
-    {'P','P','P','P','P','P','P','P'},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {' ',' ',' ',' ',' ',' ',' ',' '},
-    {'p','p','p','p','p','p','p','p'},
-    {'r','n','b','q','k','b','n','r'}
-    };
-    Show(board);
+    system("clear");
+    conclusion(board);
+    printf("\nПервый ход белых (для выхода введите 'q'): ");
+    scanf("%s", input);
+    while (input[0] != 'q') {
+        input[0] = move(input, board);
+    }
     return 0;
 }
