48bbbf7d408f4f93f0ff2c0c7216091832217c5a
Ewdokimova21 <viol.ewdokimowa@yandex.ru>
2022-03-27 23:02:35 +0700

    edit Make

---
+++ src/lib/chess.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void conclusion(char board[9][9]);
+int repeat(int status, char input[7]);
+void move_check(char input[7], char board[9][9]);
+char move(char input[7], char board[9][9]);
---
+++ src/main_fail/main.c
@@ -0,0 +1,27 @@
+#include <libchessviz/chessviz.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char board[9][9]
+            = {"8rnbqkbnr",
+               "7pppppppp",
+               "6........",
+               "5........",
+               "4........",
+               "3........",
+               "2PPPPPPPP",
+               "1RNBQKBNR",
+               " abcdefgh"};
+    char input[7];
+
+    system("clear");
+    conclusion(board);
+    printf("\nПервый ход белых (для выхода введите 'q'): ");
+    scanf("%s", input);
+    while (input[0] != 'q') {
+        input[0] = move(input, board);
+    }
+    return 0;
+}
